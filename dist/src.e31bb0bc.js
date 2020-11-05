5b13c09b5dc0a901bb667e3c83712b4cbfb6563d
Tania <tania@DESKTOP-A941MPB.localdomain>
2020-11-05 17:24:14 +0100

    canviar zoe per tzoe

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -159,7 +159,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36013" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44327" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
